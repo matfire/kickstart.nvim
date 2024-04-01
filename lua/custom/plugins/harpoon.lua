1fc9a0dff216096e1aa3e3827c0f6abe727de057
Matteo Gassend <mgassend@gmail.com>
2024-04-01 10:50:36 +0200

    add: harpoon

---
+++ lua/custom/plugins/harpoon.lua
@@ -0,0 +1,39 @@
+return {
+  'ThePrimeagen/harpoon',
+  branch = 'harpoon2',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+  },
+  config = function()
+    require('harpoon').setup()
+  end,
+  keys = function()
+    local keys = {
+      {
+        '<leader>H',
+        function()
+          require('harpoon'):list():append()
+        end,
+        desc = '[H]arpoon File',
+      },
+      {
+        '<leader>h',
+        function()
+          local harpoon = require 'harpoon'
+          harpoon.ui:toggle_quick_menu(harpoon:list())
+        end,
+        desc = '[H]arpoon Quick Menu',
+      },
+    }
+    for i = 1, 5 do
+      table.insert(keys, {
+        '<leader>' .. i,
+        function()
+          require('harpoon'):list():select(i)
+        end,
+        desc = 'Harpoon to File ' .. i,
+      })
+    end
+    return keys
+  end,
+}
